4e7440da45b719e171eaa63230963a4c3ab05c34
Paloverde702 <64057536+Paloverde702@users.noreply.github.com>
2020-05-18 10:22:07 +0800

    Update RemoteShell.cmd

--- RemoteShell.cmd
+++ RemoteShell.cmd
@@ -1,11 +1,13 @@
 @echo off 
-title Remote Shell 
+title Windows Remote Shell (WinRS)
 :Remote 
 cls 
 color 0F 
-whoami 
+whoami
+time /t
+date /t
 echo ==================================================== 
-echo Windows Remote Shell - to exit remote shell use exit 
+echo Windows Remote Shell (WinRS) - end remote with: exit
 echo ==================================================== 
 set "computername=" 
 set /p computername= "Enter PC Name: " 
@@ -18,14 +20,14 @@ CHOICE /C ab /M "A - Use HTTP | B - Use HTTPS"
 IF %ERRORLEVEL% EQU 2 goto :HTTPS 
 IF %ERRORLEVEL% EQU 1 goto :HTTP 
 :HTTP 
-title Remote Shell %computername% 
+title Windows Remote Shell (WinRS) %computername% 
 winrs.exe /r:%computername% /d:C:\ cmd 
 goto :Header 
 :HTTPS 
-title Remote Shell SSL %computername% 
+title Windows Remote Shell (WinRS) SSL %computername% 
 winrs.exe /r:%computername% /usessl /d:C:\ cmd 
 :Header 
-title Remote Shell 
+title Windows Remote Shell (WinRS)
 set "computername=" 
 CHOICE /C cd /M "C - Target another computer | D - Exit shell" 
 IF %ERRORLEVEL% EQU 2 goto :Exit 
